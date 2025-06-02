90bee48f3bd986e894e7d07bab12fd7d1081f3a2
Šárka F. <sarka.flidrova@email.cz>
2025-06-02 14:24:59 +0200

    Outputting data in a table and adding currency pipe

--- src/app/investments-results/investments-results.component.ts
+++ src/app/investments-results/investments-results.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { ResultsData } from '../investment-input.model';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-investments-results',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './investments-results.component.html',
   styleUrl: './investments-results.component.css',
 })
